405b2429a5ce451e533c334feff36370e7807285
Magda Balun <magi@op.pl>
2018-01-04 14:06:00 +0100

    Change TimeChange.js file

--- modules/TimeChange.js
+++ modules/TimeChange.js
@@ -1,12 +1,12 @@
-var os = require('os');
+function changeTimeShowing(time) {
+    
+    var hours = Math.floor(time % 3600);
+    var minutes = Math.floor((time % 3600) / 60);
+    var seconds = Math.floor(time % 60);
 
-function changeTimeShowing() {
-    var time = os.uptime();
-    var hours = Math.floor(time / 3600);
-    var minutes = Math.floor((time - (hours * 3600)) / 60);
-    var seconds = Math.floor(time - (hours * 3600) - (minutes * 60)).toFixed(0);
-
-    return (hours + ' hours ' + minutes + ' min ' + seconds + ' sec');
+    return hours + ' hours ' + minutes + ' min ' + seconds + ' sec';
 }
 
 exports.print = changeTimeShowing;
+
+
